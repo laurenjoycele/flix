a7ce4a743683e55b950cb1f5ae17012dd08ccc5e
Lauren Le <laurenjle@fb.com>
2019-06-26 11:02:26 -0700

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by laurenjle on 6/26/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flix
+//
+//  Created by laurenjle on 6/26/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
